68804aa775e780a3f41a431bcf42060326e50a3d
Andrés <andresvrif@gmail.com>
2015-02-19 20:51:30 -0500

    Prueba de Hibernate
    Se probo la conexión entre Hibernate y Mysql

---
+++ testHibernate/testHibernate/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace testHibernate
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var sessionFactory = new Configuration().Configure().BuildSessionFactory();
+            var currentSession = sessionFactory.OpenSession();
+            ITransaction tx = currentSession.BeginTransaction();
+
+            /*
+            var p = new test();
+            p.nombre = "deborah";
+            currentSession.Save(p);
+            tx.Commit();
+            */
+          
+                
+            var dd = currentSession.QueryOver<test>().Where(t => t.nombre == "andres").SingleOrDefault();
+            Label1.Text = dd.nombre;
+            currentSession.Close();            
+
+        }
+    }
+}
---
+++ testHibernate/testHibernate/test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testHibernate
+{
+    public class test
+    {
+        private string NOMBRE;
+        private System.Int32 ID;
+
+        public virtual string nombre
+        {
+            get { return NOMBRE; }
+            set { NOMBRE = value; }
+        }
+
+        public virtual System.Int32 id
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+    }
+}
